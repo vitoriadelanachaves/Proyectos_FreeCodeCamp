247a7a0abfd732bb35608201563fa45c2bdd8a6d
vitoriadelanachaves <vitoriadelanachaves@gmail.com>
2024-12-05 12:28:40 +0100

    creando primeros componentes

---
+++ testimonios_freecodecamp/src/componentes/Testimonio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Testimonio()
+{
+    return
+    (
+        <div className='contenedor-testimonio'>
+            <img className='imagen-testimonio' src={require('../imagenes/testimonio-emma.png')}/>
+        </div>
+    );
+};
