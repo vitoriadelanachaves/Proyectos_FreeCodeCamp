bd59709f2287c2bf3bf79dd76f04c2db8e959b4d
vitoriadelanachaves <vitoriadelanachaves@gmail.com>
2024-12-06 16:55:39 +0100

    creando segundo proyecto + alteracion en el negrito

--- testimonios_freecodecamp/src/App.js
+++ testimonios_freecodecamp/src/App.js
@@ -20,7 +20,7 @@ function App() {
           imagen = 'sarah'
           cargo = 'Ingeniera de Software'
           empresa = 'ChatDesk'
-          testimonio = 'Siempre he freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble. problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'
+          testimonio ={<><b>FreeCodeCamp fue la puerta de entrada a mi carrera</b> como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble. problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.</>}
         />
         <Testimonio
           nombre='Shawn Wang'
